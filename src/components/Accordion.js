3ac03b42e765ca78ea443a3eb417331120248aad
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-05-26 01:20:50 +0200

    FAQ

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 export default function Accordion({ question, children }) {
+  const [open, setOpen] = useState(false);
   return (
     <article className="accordion">
-      <div>{question}</div>
+      <div className="bg-secondary rounded-2 px-3 py-2" onClick={() => setOpen(!open)}>{question}</div>
+      <div className={open && 'd-none'}>
+        {children}
+      </div>
     </article>
   );
 }
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,12 +1,19 @@
 import Layout from '../components/Layout';
+import Accordion from '../components/Accordion';
 
 export default function Faq() {
   return (
     <Layout>
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-8">
+          <div className="col-12">
             <h1>Domande frequenti</h1>
+          </div>
+
+          <div className="col-12 col-md-8">
+            <Accordion question="Posso isrivermi anche se non ho un feeler con cui connettermi?">
+              Ciaooo
+            </Accordion>
           </div>
         </div>
       </div>
