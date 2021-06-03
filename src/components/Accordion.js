8e4abd425c0413766e61398d2f409acd76ae5016
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-06-03 14:57:32 +0200

    faq

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,19 +1,20 @@
 import { useState } from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@fluentui/react-icons-mdl2';
 
 export default function Accordion({ question, children }) {
   const [open, setOpen] = useState(false);
   return (
-    <article className="accordion my-3">
+    <article className={`accordion my-2 ${open && 'open'}`}>
       <div className="accordion-header" onClick={() => setOpen(!open)}>
         <div className="col">
           {question}
         </div>
-        <div className="col-auto">
-          {open ? 'C' : 'O'}
+        <div className="col-auto accordion-icon">
+          <ChevronUpIcon />
         </div>
       </div>
       <div className={`accordion-body ${
-        open && 'd-none'}`}
+        open || 'd-none'}`}
       >
         {children}
       </div>
