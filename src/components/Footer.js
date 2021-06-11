d997463c9eea07d8685fcf79fb193e5791762356
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-06-11 23:56:50 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import { ReactComponent as AtIcon } from '../images/at-sign.svg';
 import { ReactComponent as FacebookIcon } from '../images/facebook.svg';
 import { ReactComponent as TwitterIcon } from '../images/twitter.svg';
@@ -6,25 +7,42 @@ import { GooglePlayButton, PlayStoreButton } from './Download';
 
 export default function Footer() {
   return (
-    <footer className="container mt-3">
-      <div className="row justify-content-between small text-dark">
-        <div className="col-3">
-          <IgIcon />
-          <FacebookIcon />
-          <TwitterIcon />
-          <AtIcon />
+    <footer className="container-fluid mt-3 bg-primary text-white">
+      <div className="row">
+        <div className="col-12">
+          <div className="container">
+            <div className="row">
+              <div className="row justify-content-between small text-dark py-1">
+                <div className="col-3">
+                  <Link to="/">Home</Link>
+                  <Link to="/">Scopri</Link>
+                </div>
+                <div className="col-auto">
+                  <h6 className="text-white">Condizioni</h6>
 
-        </div>
-        <div className="col-auto">
-          <p>
-            Termini e condizioni    Cookie policy    Privacy policy
-          </p>
-        </div>
-        <div className="col-3">
-          <PlayStoreButton className=" d-inline-block w-50" />
-          <GooglePlayButton className="w-50 d-inline-block" />
+                </div>
+                <div className="col-3">
+                  <h6 className="text-white">Download App</h6>
+                </div>
+                <div className="col-3">
+                  <h6 className="text-white">Contatti</h6>
+                </div>
+              </div>
+              <div className="w-100 border-bottom border-white" />
+              <div className="col-12">
+                <p className="small">
+                  Feelo Europe S.A. | Via Giuseppe Candiani, 72 20138 Milano |
+                  VAT LU30726739 | società del gruppo Feelo S.p.A
+                </p>
+                <p className="small">
+                  Ⓒ 2021 Feelo. All Rights Reserved.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
     </footer>
   );
 }
