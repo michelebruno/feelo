72c0c5b9335c950b1251f4b1fa82f721160599b4
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-06-16 12:21:07 +0200

    clip path mobile second step

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,8 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import { ReactComponent as DownloadClip } from '../images/wave-10-1x.svg';
 import { ReactComponent as Wave01 } from '../images/wave-01-1x.svg';
+import { ReactComponent as Wave102x } from '../images/wave-10-2x.svg';
+import { ReactComponent as Wave012x } from '../images/wave-01-2x.svg';
 
 SwiperCore.use([Pagination, Scrollbar, Navigation, Mousewheel, Autoplay, EffectFade, Parallax]);
 
@@ -27,6 +29,8 @@ export default function Layout({
         <div className="visually-hidden position-absolute">
           <DownloadClip width={0} height={0} />
           <Wave01 width={0} height={0} />
+          <Wave012x width={0} height={0} />
+          <Wave102x width={0} height={0} />
         </div>
       </div>
     );
